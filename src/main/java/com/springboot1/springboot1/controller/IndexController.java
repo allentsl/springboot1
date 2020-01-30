d9c268c8c0bbbe5b2f3ac7192754fd1ade42dd4a
allentsl <137541276@qq.com>
2020-01-30 21:32:46 +0800

    add 文件

--- src/main/java/com/springboot1/springboot1/controller/IndexController.java
+++ src/main/java/com/springboot1/springboot1/controller/IndexController.java
@@ -1,4 +1,16 @@
 package com.springboot1.springboot1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class IndexController {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String say(HttpServletRequest request){
+        request.setAttribute("msg","hello world");
+        return "index";
+    }
 }
